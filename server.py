9155e44422da73f144ea280077e03e6bb36d1180
Nathan Bergey <nathan.bergey@gmail.com>
2012-06-26 15:40:18 -0700

    basic gallery functionality

--- server.py
+++ server.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 import web
 import views.photos
+import views.home
 
 urls = (
-    '/', 'index',
-    '/favicon.ico', 'favicon'
+  '/', 'index',
+  '/gallery/(.+)', 'gallery'
 )
 
 class index:
-    def GET(self):
-        return views.photos.render.base(views.photos.gallery(), title="New ISS Images")
+  def GET(self):
+    return views.home.render.base(views.home.pack_homepage(), title="Browse EOL")
+    #return views.photos.render.base(views.photos.gallery(), title="New ISS Images")
 
-class favicon:
-    def GET(self):
-        # redirect to the static file ...
-        raise web.seeother('/static/favicon.ico')
-        
+class gallery:
+  def GET(self, gal):
+    return views.home.render.base(views.photos.gallery(int(gal)), title="Gallery")
+    
+
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.internalerror = web.debugerror
-    app.run()
+  app = web.application(urls, globals())
+  app.internalerror = web.debugerror
+  app.run()
