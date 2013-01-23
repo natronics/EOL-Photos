cbe044583238340bf21d574ee5988c638fabc513
Nathan Bergey <nathan.bergey@gmail.com>
2013-01-22 22:10:07 -0800

    Revert "basic gallery functionality"
    This reverts commit 9155e44422da73f144ea280077e03e6bb36d1180.

--- server.py
+++ server.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
 import web
 import views.photos
-import views.home
 
 urls = (
-  '/', 'index',
-  '/gallery/(.+)', 'gallery'
+    '/', 'index',
+    '/favicon.ico', 'favicon'
 )
 
 class index:
-  def GET(self):
-    return views.home.render.base(views.home.pack_homepage(), title="Browse EOL")
-    #return views.photos.render.base(views.photos.gallery(), title="New ISS Images")
+    def GET(self):
+        return views.photos.render.base(views.photos.gallery(), title="New ISS Images")
 
-class gallery:
-  def GET(self, gal):
-    return views.home.render.base(views.photos.gallery(int(gal)), title="Gallery")
-    
-
+class favicon:
+    def GET(self):
+        # redirect to the static file ...
+        raise web.seeother('/static/favicon.ico')
+        
 if __name__ == "__main__":
-  app = web.application(urls, globals())
-  app.internalerror = web.debugerror
-  app.run()
+    app = web.application(urls, globals())
+    app.internalerror = web.debugerror
+    app.run()
--- views/photos.py
+++ views/photos.py
@@ -1,16 +1,8 @@
 import web
-import db.sqlite as db
 
 render = web.template.render('templates/', cache=False, globals={})
 render._keywords['globals']['render'] = render
 
-def gallery(galid):
-  urls    = []
-  images  = db.get_images(galid)
-  
-  for image in images[0:100]:
-    baseurl = "http://eol.jsc.nasa.gov/sseop/images/thumb/%s/%s-%s-%d.jpg"
-    url     = baseurl % (image['mission'], image['mission'], image['roll'], image['frame'])
-    urls.append(url)
-    
-  return render.gallery(urls)
+def gallery(**k):
+  images = range(100)
+  return render.gallery(images)
