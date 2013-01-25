a8e74ece0e86756c8f874863b2bdcdf7845589f3
Nathan Bergey <nathan.bergey@gmail.com>
2013-01-25 13:54:29 -0800

    jquery loading gallery with demo image

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, render_template, send_from_directory
+from flask import Flask, request, jsonify, render_template, send_from_directory
 app = Flask(__name__)
 
 GLOBALS = {"sitename": "EOL Browser"}
@@ -12,6 +12,13 @@ def index():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/loader.html", methods=['POST'])
+def loader():
+    print request.form["id"]
+
+    images = range(10)
+    return render_template('loader.html', images=images)
 
 if __name__ == "__main__":
     app.debug = True
