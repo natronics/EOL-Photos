f7f9ab93826542be0dc5eab781c914190c5c61a9
Nathan Bergey <nathan.bergey@gmail.com>
2013-02-03 20:47:53 -0800

    updater script has optional parameter for url to past collections

--- update.py
+++ update.py
@@ -1,35 +1,47 @@
 import feedparser
 import redis
 import os
+import sys
 import json
 import eol
 
 REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 r = redis.StrictRedis.from_url(REDIS_URL)
 
-print "Checking EOL for updates"
+def update_guid(guid):
+    # check to see if set is new
+    if not r.sismember('eol-image-sets', guid):
 
-url = "http://eol.jsc.nasa.gov/sseop/NewPhotos.xml"
-d = feedparser.parse(url)
+        print "Found new photos"
 
-# Find the new image RSS entry in the list
-for entry in d.entries:
-    if '-Images' in entry.guid:
-        guid = int(entry.guid[-15:-7])
+        # get photo ids
+        link = eol.SETURL_BASE.format(guid=guid)
+        photos = eol.scrape_photos(link)
 
-        # check to see if set is new
-        if not r.sismember('eol-image-sets', guid):
+        # push into redis
+        for photo in photos:
+            r.lpush('eol-'+str(guid), json.dumps(photo))
 
-            print "Found new photos"
+    r.sadd('eol-image-sets', guid)
 
-            # get photo ids
-            link = eol.SETURL_BASE.format(guid=guid)
-            photos = eol.scrape_photos(link)
+def check_rss():
+    print "Checking EOL for updates"
 
-            # push into redis
-            for photo in photos:
-                r.lpush('eol-'+str(guid), json.dumps(photo))
+    url = "http://eol.jsc.nasa.gov/sseop/NewPhotos.xml"
+    d = feedparser.parse(url)
 
-        r.sadd('eol-image-sets', guid)
+    # Find the new image RSS entry in the list
+    for entry in d.entries:
+        if '-Images' in entry.guid:
+            guid = int(entry.guid[-15:-7])
+            update_guid(guid)
+
+
+if len(sys.argv) == 2:
+    url = sys.argv[1]
+    guid = url[-15:-7]
+    update_guid(guid)
+else:
+    check_rss()
 
 print "Finished"
