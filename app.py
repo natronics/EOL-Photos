b1faaf9105ebc708676968ec53441bcba50fd523
Nathan Bergey <nathan.bergey@gmail.com>
2013-12-11 20:32:11 -0800

    kind of proof of concept posgres

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
 
+app.debug = True
+
+
+class PhotoSet(db.Model):
+    id = db.Column(db.String(80), primary_key=True)
+    date = db.Column(db.Date)
+
+    def __repr__(self):
+        return '<set %r>' % self.id
 
 @app.route("/")
 def index():
-    return "hello"
+    resp =  "db: " + os.getenv('DATABASE_URL')
+    for s in PhotoSet.query.all():
+        resp += "\n" + s
+
+    return resp
 
 
 @app.route('/favicon.ico')
